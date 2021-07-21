104403040f1d7cfeddff2cdd02d1a79d26417799
gowufang <gowufang@gmail.com>
2021-07-21 20:19:32 +0800

    init kg

---
+++ edu/src/main/java/io/agora/education/api/stream/data/VideoEncoderConfig.kt
@@ -0,0 +1,101 @@
+package io.agora.education.api.stream.data
+
+import android.os.Parcel
+import android.os.Parcelable
+
+enum class OrientationMode() : Parcelable {
+
+    ADAPTIVE,
+    FIXED_LANDSCAPE,
+    FIXED_PORTRAIT;
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(ordinal)
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<OrientationMode> {
+        override fun createFromParcel(parcel: Parcel): OrientationMode {
+            return OrientationMode.values()[parcel.readInt()]
+        }
+
+        override fun newArray(size: Int): Array<OrientationMode?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+enum class DegradationPreference() : Parcelable {
+    MAINTAIN_QUALITY,
+    MAINTAIN_FRAME_RATE,
+    MAINTAIN_BALANCED;
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(ordinal)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<DegradationPreference> {
+        override fun createFromParcel(parcel: Parcel): DegradationPreference {
+            return DegradationPreference.values()[parcel.readInt()]
+        }
+
+        override fun newArray(size: Int): Array<DegradationPreference?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+object VideoDimensions {
+    val VideoDimensions_640X480 = arrayOf(640, 480)
+    val VideoDimensions_320X240 = arrayOf(320, 240)
+    val VideoDimensions_360X240 = arrayOf(360, 240)
+}
+
+data class VideoEncoderConfig(
+        var videoDimensionWidth: Int = 360,
+        var videoDimensionHeight: Int = 360,
+        var frameRate: Int = 15,
+        var bitrate: Int = 200,
+        var orientationMode: OrientationMode? = OrientationMode.ADAPTIVE,
+        var degradationPreference: DegradationPreference? = DegradationPreference.MAINTAIN_QUALITY
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readParcelable(OrientationMode::class.java.classLoader),
+            parcel.readParcelable(DegradationPreference::class.java.classLoader)) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(videoDimensionWidth)
+        parcel.writeInt(videoDimensionHeight)
+        parcel.writeInt(frameRate)
+        parcel.writeInt(bitrate)
+        parcel.writeParcelable(orientationMode, flags)
+        parcel.writeParcelable(degradationPreference, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<VideoEncoderConfig> {
+        override fun createFromParcel(parcel: Parcel): VideoEncoderConfig {
+            return VideoEncoderConfig(parcel)
+        }
+
+        override fun newArray(size: Int): Array<VideoEncoderConfig?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
